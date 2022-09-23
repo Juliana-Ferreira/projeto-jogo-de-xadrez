3367362e3371ed2dd677c289d3389690c43428d5
Juliana-Ferreira <juliana_cristina.ferreira@hotmail.com>
2022-09-23 11:58:04 -0300

    Movimento especial ROQUE

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,18 +2,24 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece{
 
-	public King(Board board, Color color) {
+	private ChessMatch chessMatch;
+	
+	public King(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
+		this.chessMatch = chessMatch;
 	}
-
-	@Override
-	public String toString() {
-		return "K";
+	
+	private boolean testRookCastling(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p instanceof Rook
+				&& p.getColor() == getColor()
+				&& p.getMoveCount() == 0;
 	}
 
 	private boolean canMove(Position position) {
@@ -74,6 +80,35 @@ public class King extends ChessPiece{
 			matriz[p.getRow()][p.getColumn()] = true;
 		}
 		
+		// Movimento especial ROQUE
+		if(getMoveCount() == 0 && !chessMatch.getCheck()) {
+			// Roque pequeno
+			Position positionRightRook = new Position(position.getRow(), position.getColumn() + 3);
+			if(testRookCastling(positionRightRook)) {
+				Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+				if(getBoard().piece(p1) == null && getBoard().piece(p2) == null) {
+					matriz[position.getRow()][position.getColumn() + 2] = true;
+				}
+			}
+			
+			// Roque grande
+			Position positionLeftRook = new Position(position.getRow(), position.getColumn() - 4);
+			if(testRookCastling(positionLeftRook)) {
+				Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+				Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+				if(getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
+					matriz[position.getRow()][position.getColumn() - 2] = true;
+				}
+			}
+		}
+		
 		return matriz;
+	}
+	
+	@Override
+	public String toString() {
+		return "K";
 	}
 }
